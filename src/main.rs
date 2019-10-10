0818d06132e8d44f2c312c4fd167c405478ce834
Leonard Lorenz <bi0m3tr1xx@gmail.com>
2019-10-10 13:21:31 +0200

    reading environment variables currentdir and LEAFPATH. For now just printing paths, still have to improve error handling

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 const TEST_SOURCECODE: &[u8] = b"
 type Coordinate
     x int
-    y int 
+    y int
 
 fn main
    double << add 20 << 20 + 20
@@ -10,11 +10,15 @@ fn main
 
 mod parser;
 use parser::Parser;
+use std::path::Path;
 
 pub mod datatypes;
 
 fn main() {
+
     let mut parser = Parser::new();
+
+    read_prelude_source();
 
     println!("{}\n", String::from_utf8(TEST_SOURCECODE.to_vec()).unwrap());
 
@@ -45,4 +49,24 @@ fn main() {
     let func = parser.get_function(MAIN_MODULE_ID, "main");
     evaler.run(func);
     */
+}
+
+fn read_prelude_source() {
+    let env_leafpath = std::env::var("LEAFPATH").expect("LEAFPATH variable not set.");
+    // get path object from LEAFPATH environment variable string
+    let leafpath : &Path = Path::new(&env_leafpath);
+
+    // get current working directory from environment variable
+    let env_current_dir = std::env::current_dir().expect("Current directory couldn't be read.");
+    let current_dir : &Path = env_current_dir.as_path();
+
+    tokenize_prelude(leafpath.join("prelude").as_path());
+    tokenize_prelude(current_dir.join("prelude").as_path());
+}
+
+fn tokenize_prelude(path : &Path) {
+    for entry in std::fs::read_dir(path).expect("Couldn't read directory") {
+        let file = entry.expect("Couldn't read file.");
+        println!("{:?}", file.path());
+    }
 }

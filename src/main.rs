f467c8bfd0e601899f295e83036a041c4893b757
Leonard Lorenz <bi0m3tr1xx@gmail.com>
2019-10-11 09:51:45 +0200

    made read_prelude_source() methods of parser.rs

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,6 @@ fn main
 
 mod parser;
 use parser::Parser;
-use std::path::Path;
 
 pub mod datatypes;
 
@@ -18,7 +17,7 @@ fn main() {
 
     let mut parser = Parser::new();
 
-    read_prelude_source();
+    parser.read_prelude_source();
 
     println!("{}\n", String::from_utf8(TEST_SOURCECODE.to_vec()).unwrap());
 
@@ -49,24 +48,4 @@ fn main() {
     let func = parser.get_function(MAIN_MODULE_ID, "main");
     evaler.run(func);
     */
-}
-
-fn read_prelude_source() {
-    let env_leafpath = std::env::var("LEAFPATH").expect("LEAFPATH variable not set.");
-    // get path object from LEAFPATH environment variable string
-    let leafpath : &Path = Path::new(&env_leafpath);
-
-    // get current working directory from environment variable
-    let env_current_dir = std::env::current_dir().expect("Current directory couldn't be read.");
-    let current_dir : &Path = env_current_dir.as_path();
-
-    tokenize_prelude(leafpath.join("prelude").as_path());
-    tokenize_prelude(current_dir.join("prelude").as_path());
-}
-
-fn tokenize_prelude(path : &Path) {
-    for entry in std::fs::read_dir(path).expect("Couldn't read directory") {
-        let file = entry.expect("Couldn't read file.");
-        println!("{:?}", file.path());
-    }
 }
--- src/parser.rs
+++ src/parser.rs
@@ -11,6 +11,7 @@ pub use function::{BodySource, FunctionBuilder};
 mod r#type;
 pub use r#type::Type;
 pub mod flags;
+use std::path::Path;
 
 pub struct Parser {
     module_ids: HashMap<String, usize>,
@@ -94,6 +95,26 @@ impl Parser {
                 module.type_fields.push(fields);
                 typeid
             }
+        }
+    }
+
+    pub fn read_prelude_source(&mut self) {
+        let env_leafpath = std::env::var("LEAFPATH").expect("LEAFPATH variable not set.");
+        // get path object from LEAFPATH environment variable string
+        let leafpath : &Path = Path::new(&env_leafpath);
+
+        // get current working directory from environment variable
+        let env_current_dir = std::env::current_dir().expect("Current directory couldn't be read.");
+        let current_dir : &Path = env_current_dir.as_path();
+
+        self.tokenize_prelude(leafpath.join("prelude").as_path());
+        self.tokenize_prelude(current_dir.join("prelude").as_path());
+    }
+
+    fn tokenize_prelude(&mut self, path : &Path) {
+        for entry in std::fs::read_dir(path).expect("Couldn't read directory") {
+            let file = entry.expect("Couldn't read file.");
+            println!("{:?}", file.path());
         }
     }
 

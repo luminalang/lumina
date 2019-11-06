6583f2928e9e1633ba72b33a4495b89c39a54464
Simon Larsson <simon.larsson-general@protonmail.com>
2019-11-06 21:30:01 +0100

    First Statements in the evaluation engine

--- src/interpreter/runner.rs
+++ src/interpreter/runner.rs
@@ -1,5 +1,5 @@
 use super::runtime::Runtime;
-use crate::ir::{Entity, If, Value};
+use crate::ir::{Entity, First, If, Value};
 
 mod bridge;
 mod parambuffer;
@@ -37,6 +37,7 @@ impl<'a> Runner<'a> {
                 Entity::Parameter(n) => return self.params.param_consume(*n as usize),
                 Entity::Inlined(v) => return v.clone(),
                 Entity::IfExpression(expr) => return self.if_expression(expr),
+                Entity::FirstStatement(stmt) => return self.first_statement(stmt),
                 Entity::FunctionCall(findex, params) => {
                     let evaluated_params = params
                         .iter()
@@ -53,26 +54,38 @@ impl<'a> Runner<'a> {
         }
     }
 
-    fn rust_call(&self, index: u16, rust_params: &[Entity]) -> Value {
+    fn rust_call(mut self, index: u16, rust_params: &'a [Entity]) -> Value {
         match rust_params.len() {
             2 => {
                 let func = bridge::get_func_two(index);
-                func(
-                    self.spawn(&rust_params[0], self.params.borrow()),
-                    self.spawn(&rust_params[1], self.params.borrow()),
-                )
+
+                let a = self.spawn(&rust_params[1], self.params.borrow());
+
+                self.entity = &rust_params[1];
+                let b = self.run();
+
+                func(a, b)
             }
             _ => unreachable!(),
         }
     }
-    fn if_expression(&self, expr: &If<Entity>) -> Value {
+    fn if_expression(mut self, expr: &'a If<Entity>) -> Value {
         for i in 0..expr.branches() {
             let cond = expr.condition(i);
             if let Value::Bool(true) = self.spawn(cond, self.params.borrow()) {
-                let eval = expr.evaluation(i);
-                return self.spawn(eval, self.params.borrow());
+                self.entity = expr.evaluation(i);
+                return self.run();
+                // return self.spawn(eval, self.params.borrow());
             }
         }
-        self.spawn(expr.r#else(), self.params.borrow())
+        self.entity = expr.r#else();
+        self.run()
+    }
+    fn first_statement(mut self, stmt: &'a First<Entity>) -> Value {
+        for entity in stmt.to_void() {
+            self.spawn(entity, self.params.borrow());
+        }
+        self.entity = stmt.to_eval();
+        self.run()
     }
 }

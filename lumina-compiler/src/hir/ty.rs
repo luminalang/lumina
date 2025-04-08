b625968982ca40289f977d762f226b1d559a8230
Simon Larsson <simon.larsson-general@protonmail.com>
2025-04-08 13:25:48 +0200

    use name resolution to allow sugared single-type in fn signature

--- lumina-compiler/src/hir/ty.rs
+++ lumina-compiler/src/hir/ty.rs
@@ -7,6 +7,7 @@ use lumina_typesystem::{
 };
 use lumina_util::{Spanned, Tr};
 use smallvec::SmallVec;
+use std::fmt;
 
 pub struct TypeLower<'t, 'a, 's> {
     module: key::Module,
@@ -133,7 +134,7 @@ impl<'t, 'a, 's> TypeLower<'t, 'a, 's> {
 
     pub fn typing<T: FromVar>(&mut self, typing: &ParserTyping<'s>) -> hir::Typing<Ty<T>> {
         hir::Typing {
-            params: self.tys_spanned(&typing.ptypes),
+            params: self.ty_sequence(&typing.ptypes),
             returns: self.ty_spanned(typing.returns.as_ref()),
         }
     }
@@ -230,7 +231,37 @@ impl<'t, 'a, 's> TypeLower<'t, 'a, 's> {
                 }
             }
             parser::Type::Poison => Ty::poison(),
-            parser::Type::Defined(path, params) => self.defined(path.tr(ty.span), params),
+            parser::Type::Defined(apath, params) => {
+                if let Some(name) = apath.path.as_name() {
+                    if let Some(nty) = self.try_builtin(name.tr(ty.span), params.len()) {
+                        self.check_param_count(&ty, params.len().tr(ty.span), 0);
+                        return nty;
+                    }
+                }
+
+                match self.try_resolved(ty.span, apath.path.as_slice()) {
+                    Resolved::Some(key) => {
+                        let header = &self.ast.entities.header_of_ty(key).header;
+                        let ok = self.check_param_count(
+                            header,
+                            params.len().tr(ty.span),
+                            header.type_params.len(),
+                        );
+
+                        if ok {
+                            let params = self.tys(params);
+                            Ty::defined(key, params)
+                        } else {
+                            Ty::poison()
+                        }
+                    }
+                    Resolved::Alias(nty) => {
+                        self.check_param_count(&ty, params.len().tr(ty.span), 0);
+                        return nty;
+                    }
+                    Resolved::Poison => Ty::poison(),
+                }
+            }
         }
     }
 
@@ -243,6 +274,54 @@ impl<'t, 'a, 's> TypeLower<'t, 'a, 's> {
         F: FromIterator<Tr<Ty<T>>>,
     {
         ty.iter().map(|ty| self.ty_spanned(ty.as_ref())).collect()
+    }
+
+    // In some cases (such as function signature) we want to both allow
+    // `int int -> int`
+    // and
+    // `Maybe int -> int`
+    //
+    // However; these are ambigious and need type resolution to handle properly.
+    // So; we do a sort of ad-hoc currying to resolve it if there's only a single type.
+    fn ty_sequence<T: FromVar, F>(&mut self, tys: &[Tr<parser::Type<'s>>]) -> F
+    where
+        F: FromIterator<Tr<Ty<T>>>,
+    {
+        if tys.is_empty() {
+            return [].into_iter().collect();
+        }
+
+        match &tys[0].value {
+            parser::Type::Defined(apath, params) if params.is_empty() => {
+                let as_first = |this: &mut Self, nty: Ty<T>| {
+                    this.check_param_count(&tys[0], params.len().tr(tys[0].span), 0);
+                    let then = this.tys_spanned::<Vec<_>, T>(&tys[1..]);
+                    [nty.tr(tys[0].span)].into_iter().chain(then).collect()
+                };
+
+                if let Some(name) = apath.path.as_name() {
+                    if let Some(nty) = self.try_builtin(name.tr(tys[0].span), params.len()) {
+                        return as_first(self, nty);
+                    }
+                }
+
+                match self.try_resolved::<T>(tys[0].span, apath.path.as_slice()) {
+                    Resolved::Some(key) => {
+                        let header = &self.ast.entities.header_of_ty(key).header;
+                        if header.type_params.is_empty() {
+                            as_first(self, Ty::defined(key, vec![]))
+                        } else {
+                            let params = self.tys::<T>(&tys[1..]);
+                            let tspan = Span::from_elems(tys, |v| v.span);
+                            [Ty::defined(key, params).tr(tspan)].into_iter().collect()
+                        }
+                    }
+                    Resolved::Alias(ty) => as_first(self, ty),
+                    Resolved::Poison => as_first(self, Ty::poison()),
+                }
+            }
+            _ => self.tys_spanned(tys),
+        }
     }
 
     pub fn resolve_array_generic(&mut self, name: Tr<&'s str>) -> Option<Generic> {
@@ -262,11 +341,77 @@ impl<'t, 'a, 's> TypeLower<'t, 'a, 's> {
             .ok()
     }
 
-    fn defined<T: FromVar>(
-        &mut self,
-        apath: Tr<&parser::AnnotatedPath<'s>>,
-        params: &[Tr<parser::Type<'s>>],
-    ) -> Ty<T> {
+    fn try_builtin<T: FromVar>(&mut self, name: Tr<&'s str>, plen: usize) -> Option<Ty<T>> {
+        self.try_prim(name)
+            .or_else(|| self.try_generic(*name, plen, None).ok().map(Ty::Generic))
+    }
+
+    fn try_prim<T: FromVar>(&mut self, path: Tr<&'s str>) -> Option<Ty<T>> {
+        match *path {
+            "_" => Some(self.inference(path.span)),
+            "bool" => Some(Ty::bool()),
+            "int" => Some(Ty::Int(IntSize::new(true, self.default_int_size))),
+            "uint" => Some(Ty::Int(IntSize::new(false, self.default_int_size))),
+            "float" => Some(Ty::f64()),
+            "self" => match self.type_info.self_handler {
+                SelfHandler::Substituted(kind) => {
+                    let (forall, gkind) = &self.type_info.cforalls.last().unwrap();
+                    assert_eq!(*gkind, GenericKind::Entity);
+                    let params = forall.to_types(*gkind);
+                    Some(Ty::defined(kind, params))
+                }
+                SelfHandler::Direct => return Some(Ty::Simple("self")),
+                SelfHandler::Disallowed => {
+                    self.ast
+                        .sources
+                        .error("invalid type")
+                        .m(self.module)
+                        .eline(
+                            path.span,
+                            "`self` is only allowed in type and implementation declarations",
+                        )
+                        .emit();
+
+                    Some(Ty::poison())
+                }
+            },
+            "string" => Some(Ty::string(self.type_info.string, vec![])),
+            name if name.starts_with('u') => name[1..]
+                .parse::<u8>()
+                .ok()
+                .map(|n| Ty::Int(IntSize::new(false, n))),
+            name if name.starts_with('i') => name[1..]
+                .parse::<u8>()
+                .ok()
+                .map(|n| Ty::Int(IntSize::new(true, n))),
+            _ => None,
+        }
+    }
+
+    fn inference<T: FromVar>(&mut self, span: Span) -> Ty<T> {
+        match self.type_info.inference.as_mut() {
+            None => {
+                self.ast
+                    .sources
+                    .error("invalid type")
+                    .m(self.module)
+                    .eline(span, "type inference not allowed in this context")
+                    .emit();
+
+                return Ty::poison();
+            }
+            Some(vars) => {
+                let var = vars.var(span);
+                if self.type_info.declare_generics {
+                    vars.enable_lift_to_generic(var);
+                }
+                // TODO: rememember to fix forbid_params in upper scope
+                return T::var(var);
+            }
+        }
+    }
+
+    fn warn_useless_annotation(&self, apath: Tr<&parser::AnnotatedPath<'s>>) {
         if !apath.for_segments.is_empty() {
             self.ast.sources
                 .warning("invalid type annotation")
@@ -275,139 +420,15 @@ impl<'t, 'a, 's> TypeLower<'t, 'a, 's> {
                 .text("if you're attempting to annotate an associated type of a trait constraint, they are very experimental")
                 .emit();
         }
+    }
 
-        let span = apath.span;
-        let path = apath.path.as_slice();
-
-        match path {
-            ["_"] => match self.type_info.inference.as_mut() {
-                None => {
-                    self.ast
-                        .sources
-                        .error("invalid type")
-                        .m(self.module)
-                        .eline(span, "type inference not allowed in this context")
-                        .emit();
-
-                    return Ty::poison();
-                }
-                Some(vars) => {
-                    let var = vars.var(span);
-                    if self.type_info.declare_generics {
-                        vars.enable_lift_to_generic(var);
-                    }
-                    let ty = T::var(var);
-                    return self.forbid_params(span, ty, params);
-                }
-            },
-            ["bool"] => {
-                return self.forbid_params(span, Ty::bool(), params);
-            }
-            ["int"] => {
-                let int = Ty::Int(IntSize::new(true, self.default_int_size));
-                return self.forbid_params(span, int, params);
-            }
-            ["uint"] => {
-                let int = Ty::Int(IntSize::new(false, self.default_int_size));
-                return self.forbid_params(span, int, params);
-            }
-            ["float"] => {
-                return self.forbid_params(span, Ty::f64(), params);
-            }
-            ["self"] => match self.type_info.self_handler {
-                SelfHandler::Substituted(kind) => {
-                    let (forall, gkind) = &self.type_info.cforalls.last().unwrap();
-                    assert_eq!(*gkind, GenericKind::Entity);
-                    let params = forall.to_types(*gkind);
-                    return Ty::defined(kind, params);
-                }
-                SelfHandler::Direct => return Ty::Simple("self"),
-                SelfHandler::Disallowed => {
-                    self.ast
-                        .sources
-                        .error("invalid type")
-                        .m(self.module)
-                        .eline(
-                            span,
-                            "`self` is only allowed in type and implementation declarations",
-                        )
-                        .emit();
-
-                    return Ty::poison();
-                }
-            },
-            ["string"] => {
-                let string = self.type_info.string;
-                return self.forbid_params(span, Ty::string(string, vec![]), params);
-            }
-            [name] if name.starts_with('u') => {
-                if let Ok(n) = name[1..].parse::<u8>() {
-                    let int = Ty::Int(IntSize::new(false, n));
-                    return self.forbid_params(span, int, params);
-                }
-            }
-            [name] if name.starts_with('i') => {
-                if let Ok(n) = name[1..].parse::<u8>() {
-                    let int = Ty::Int(IntSize::new(true, n));
-                    return self.forbid_params(span, int, params);
-                }
-            }
-            [name] => match self.try_generic(name, params.len(), None) {
-                Ok(gen) => {
-                    return self.forbid_params(span, Ty::Generic(gen), params);
-                }
-                Err(GenericError::InconsistentHKT(exp)) => {
-                    self.emit_inconsistent_hkt(span, exp, params.len());
-                    return Ty::poison();
-                }
-                Err(GenericError::NotFound) => {}
-            },
-            _ => {}
-        }
-
+    fn try_resolved<T: FromVar>(&mut self, span: Span, path: &[&'s str]) -> Resolved<T> {
         match self.ast.lookups.resolve_type(self.module, path) {
             Ok(entity) => match entity.key {
-                Entity::Alias(ty) => return self.ty(ty.as_ref()),
+                Entity::Alias(ty) => Resolved::Alias(self.ty(ty.as_ref())),
                 Entity::Type(tkey) => {
                     let key = M(entity.module, tkey);
-                    let mut tparams = self.tys(params);
-                    let expected_type_params =
-                        &self.ast.entities.header_of_ty(key).header.type_params;
-
-                    let err = |span, msg| {
-                        self.ast
-                            .sources
-                            .error("invalid type")
-                            .m(self.module)
-                            .eline(span, msg)
-                            .emit()
-                    };
-
-                    let header = || &self.ast.entities.header_of_ty(key).header;
-
-                    match (params.len(), expected_type_params.len()) {
-                        (x, y) if x < y => {
-                            err(
-                                span,
-                                format!("missing {} type parameters for `{}`", y - x, header()),
-                            );
-
-                            while tparams.len() != expected_type_params.len() {
-                                tparams.push(Ty::poison());
-                            }
-                        }
-                        (x, y) if x > y => {
-                            err(
-                                params[params.len() - 1].span,
-                                format!("excess parameter for {}", header()),
-                            );
-
-                            tparams.truncate(expected_type_params.len());
-                        }
-                        (_, _) => {}
-                    }
-
-                    Ty::defined(key, tparams)
+                    Resolved::Some(key)
                 }
                 _ => {
                     let name = path.last().unwrap();
@@ -415,7 +436,7 @@ impl<'t, 'a, 's> TypeLower<'t, 'a, 's> {
                         .sources
                         .emit_wrong_entity(self.module, span, name, "type", entity.key);
 
-                    Ty::poison()
+                    Resolved::Poison
                 }
             },
 
@@ -424,8 +445,31 @@ impl<'t, 'a, 's> TypeLower<'t, 'a, 's> {
                     .sources
                     .emit_lookup_err(span, self.module, "type", err);
 
-                Ty::poison()
+                Resolved::Poison
             }
+        }
+    }
+
+    fn check_param_count(&mut self, header: impl fmt::Display, got: Tr<usize>, exp: usize) -> bool {
+        let err = |span, msg| {
+            self.ast
+                .sources
+                .error("invalid type")
+                .m(self.module)
+                .eline(span, msg)
+                .emit();
+            false
+        };
+
+        if *got < exp {
+            err(
+                got.span,
+                format!("missing {} type parameters for `{header}`", exp - *got),
+            )
+        } else if *got > *got {
+            err(got.span, format!("excess parameter for `{header}`"))
+        } else {
+            true
         }
     }
 
@@ -496,6 +540,12 @@ impl<'t, 'a, 's> TypeLower<'t, 'a, 's> {
 
         Err(GenericError::NotFound)
     }
+}
+
+enum Resolved<T> {
+    Some(M<key::TypeKind>),
+    Alias(Ty<T>),
+    Poison,
 }
 
 pub enum GenericError {
